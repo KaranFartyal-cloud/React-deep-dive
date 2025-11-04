733d3441789964ab642d92bf2fca6dbfb51619a4
karan fartyal <karanfartyaloffice@gmail.com>
2025-11-04 18:54:36 +0530

    implement rendering with debouncing

--- src/components/Child1.tsx
+++ src/components/Child1.tsx
@@ -10,11 +10,9 @@ const Child1: React.FC<Props> = ({ name }) => {
   return (
     <div>
       <div>{name}</div>
-      <div>
-        <Child2 />
-      </div>
+      
     </div>
   );
 };
 
-export default Child1;
+export default React.memo(Child1);
--- src/components/ParentComponent.tsx
+++ src/components/ParentComponent.tsx
@@ -1,16 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Child1 from "./Child1";
+import Child2 from "./Child2";
 
 const ParentComponent = () => {
   const [name, setName] = useState("");
+  const [debouncedName, setDebouncedName] = useState("");
   console.log("parent component has been re rendered");
+
+  // update input value immediately
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  // debounce logic inside useEffect
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedName(name);
+    }, 1000); // wait 1 second after last keystroke
+
+    return () => {
+      clearTimeout(handler); // cancel previous timer
+    };
+  }, [name]);
+
   return (
     <div>
-      <Child1 name={name} />
-      <br></br>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <Child1 name={debouncedName} />
+      <br />
+      <input
+        type="text"
+        value={name}
+        onChange={handleChange}
+        placeholder="Type your name"
+      />
+      <div>
+        <Child2 />
+      </div>
     </div>
   );
 };
 
-export default ParentComponent;
+export default React.memo(ParentComponent);
